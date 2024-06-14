c3c2b4f2bb036d267e59f15b1beed0f677060cf4
Utkarsh Jaiswal <starutkarshj017@gmail.com>
2024-06-14 19:55:13 +0530

    cartPage fixed

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -116,20 +116,16 @@ const Cart = (props) => {
                 {temp_pdt && temp_pdt.length > 0 && temp_pdt.map((item, index) => {
                     return (
                         <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <a href="#">
                                 <img height=''
                                     className="p-8 rounded-t-lg"
                                     src={'http://localhost:3000/' + item.image}
                                     alt="product image"
                                 />
-                            </a>
                             <div className="px-5 pb-5">
-                                <a href="#">
                                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                                         <span >{item.name}</span> | {item.category}
 
                                     </h5>
-                                </a>
                                 <div className="flex items-center mt-2.5 mb-5">
                                     <div className="flex items-center space-x-1 rtl:space-x-reverse">
                                         <svg
@@ -146,7 +142,11 @@ const Cart = (props) => {
                                     <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
                                         5.0
                                     </span>
+                                    
                                 </div>
+                                <div className="flex items-center justify-between">
+                                    <span className=" text-3xl font-bold text-gray-900 dark:text-white">Rs.{item.price}</span>
+                                    </div>
                             </div>
                         </div>
                     )
